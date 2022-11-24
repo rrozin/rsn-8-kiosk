d99f38d2a583ccba1b13685567a641ff4a4f4e61
rrozin <rrozin@yahoo.com>
2022-11-23 16:09:16 -0800

    adding refresh shell script

--- shell-scripts/refresh-browser.sh
+++ shell-scripts/refresh-browser.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "Hello World"
+export DISPLAY=:0.0
+export XAUTHORITY=/home/pi/.Xauthority
+
+while true;
+do
+  xdotool key ctrl+r &
+
+  sleep 1 #refresh time in seconds
+done
+
+echo "Refreshing Browser"
